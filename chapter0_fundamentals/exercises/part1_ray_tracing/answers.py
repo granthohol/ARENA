b7a655a83a2e39e004403fc2b9d42ba3c1636152
Grant Hohol <ghohol@wisc.edu>
2024-11-15 12:45:00 -0600

    Ray Tracing Code

---
+++ chapter0_fundamentals/exercises/part1_ray_tracing/answers.py
@@ -0,0 +1,121 @@
+#%%
+import os
+import sys
+from pathlib import Path
+
+import einops
+import plotly.express as px
+import torch as t
+from IPython.display import display
+from ipywidgets import interact
+from jaxtyping import Bool, Float, jaxtyped
+from torch import Tensor
+from typeguard import typechecked as typechecker
+
+# Make sure exercises are in the path
+chapter = r"chapter0_fundamentals"
+exercises_dir = Path(f"{os.getcwd().split(chapter)[0]}/{chapter}/exercises").resolve()
+section_dir = exercises_dir / "part1_ray_tracing"
+if str(exercises_dir) not in sys.path: sys.path.append(str(exercises_dir))
+
+import part1_ray_tracing.tests as tests
+from part1_ray_tracing.utils import (
+    render_lines_with_plotly,
+    setup_widget_fig_ray,
+    setup_widget_fig_triangle,
+)
+from plotly_utils import imshow
+
+MAIN = __name__ == "__main__"
+
+#%% [markdown]
+## 1-D Image Rendering
+
+# %%
+def make_rays_1d(num_pixels: int, y_limit: float) -> t.Tensor:
+    '''
+    num_pixels: The number of pixels in the y dimension. Since there is one ray per pixel, this is also the number of rays.
+    y_limit: At x=1, the rays should extend from -y_limit to +y_limit, inclusive of both endpoints.
+
+    Returns: shape (num_pixels, num_points=2, num_dim=3) where the num_points dimension contains (origin, direction) and the num_dim dimension contains xyz.
+
+    Example of make_rays_1d(9, 1.0): [
+        [[0, 0, 0], [1, -1.0, 0]],
+        [[0, 0, 0], [1, -0.75, 0]],
+        [[0, 0, 0], [1, -0.5, 0]],
+        ...
+        [[0, 0, 0], [1, 0.75, 0]],
+        [[0, 0, 0], [1, 1, 0]],
+    ]
+    '''
+    shape = t.zeros((num_pixels, 2, 3), dtype=float)
+    t.linspace(-y_limit, y_limit, steps=num_pixels, out = shape[:, 1, 1])
+    shape[:, 1, 0] = 1
+    return shape
+
+rays1d = make_rays_1d(9, 10.0)
+
+fig = render_lines_with_plotly(rays1d)
+
+
+#%% [markdown]
+## Ray Object Intersection
+
+# %%
+fig = setup_widget_fig_ray()
+display(fig)
+
+@interact
+def response(seed=(0, 10, 1), v=(-2.0, 2.0, 0.01)):
+    t.manual_seed(seed)
+    L_1, L_2 = t.rand(2, 2)
+    P = lambda v: L_1 + v * (L_2 - L_1)
+    x, y = zip(P(-2), P(2))
+    with fig.batch_update(): 
+        fig.data[0].update({"x": x, "y": y}) 
+        fig.data[1].update({"x": [L_1[0], L_2[0]], "y": [L_1[1], L_2[1]]}) 
+        fig.data[2].update({"x": [P(v)[0]], "y": [P(v)[1]]})
+# %%
+segments = t.tensor([
+    [[1.0, -12.0, 0.0], [1, -6.0, 0.0]], 
+    [[0.5, 0.1, 0.0], [0.5, 1.15, 0.0]], 
+    [[2, 12.0, 0.0], [2, 21.0, 0.0]]
+])
+
+#fig = render_lines_with_plotly(rays1d)
+fig2 = render_lines_with_plotly(rays1d*3, segments)
+# %%
+def intersect_ray_1d(ray: t.Tensor, segment: t.Tensor) -> bool:
+    '''
+    ray: shape (n_points=2, n_dim=3)  # O, D points
+    segment: shape (n_points=2, n_dim=3)  # L_1, L_2 points
+
+    Return True if the ray intersects the segment.
+    '''
+    # Get the x and y coordinates (ignore z)
+    ray = ray[..., :2]
+    segment = segment[..., :2]
+
+    # Ray is [[Ox, Oy], [Dx, Dy]]
+    O, D = ray
+    # Segment is [[L1x, L1y], [L2x, L2y]]
+    L_1, L_2 = segment
+
+    # Create matrix and vector, and solve equation
+    mat = t.stack([D, L_1 - L_2], dim=-1)
+    vec = L_1 - O
+
+    # Solve equation (return False if no solution)
+    try:
+        sol = t.linalg.solve(mat, vec)
+    except RuntimeError:
+        return False
+
+    # If there is a solution, check the soln is in the correct range for there to be an intersection
+    u = sol[0].item()
+    v = sol[1].item()
+    return (u >= 0.0) and (v >= 0.0) and (v <= 1.0)
+
+
+tests.test_intersect_ray_1d(intersect_ray_1d)
+tests.test_intersect_ray_1d_special_case(intersect_ray_1d)
